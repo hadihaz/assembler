ba7201ab256d66150e00e8dd1d639c8a2abeeea6
Hadi <hadihaz1999@gmail.com>
2023-12-05 22:23:38 +0330

    finding the code pattern

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const { read, write } = require("./src/file");
-
+let arr = [];
+arr.push(read().split("\n"));
+arr = arr[0].map((line) => {
+  return [line.slice(0, 4).trim(), line.slice(4, 8).trim(), ...line.slice(8).trim().split(" ")];
+});
+console.log(arr);
