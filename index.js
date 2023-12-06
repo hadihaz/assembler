cc2a0017c7d9876c4179e2b2e4814707b71a6816
Hadi <hadihaz1999@gmail.com>
2023-12-06 11:59:04 +0330

    build convert hex

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { read, write } = require("./src/file");
+const convertHex = require("./src/convertHex");
 let arr = [];
 arr.push(read().split("\n"));
 arr = arr[0].map((line) => {
-  return [line.slice(0, 4).trim(), line.slice(4, 8).trim(), ...line.slice(8).trim().split(" ")];
+  return [
+    line.slice(0, 4).trim(),
+    line.slice(4, 8).trim(),
+    ...line.slice(8).trim().split(" "),
+  ];
 });
-console.log(arr);
+hex = convertHex(arr);
+console.log(hex);
---
+++ src/convertBinery.js
@@ -0,0 +1,4 @@
+function convertBinery(hex){
+
+}
+
